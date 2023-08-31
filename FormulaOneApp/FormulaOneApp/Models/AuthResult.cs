057bd5f08cbd76594933f2e19fdb3804dc9fa1bf
Harsh Gupta <guptaharsh686@gmail.com>
2023-09-01 01:30:58 +0530

    Added DTOS for incomming registeration request and outgoing authentication request

---
+++ FormulaOneApp/FormulaOneApp/Models/AuthResult.cs
@@ -0,0 +1,9 @@
+﻿namespace FormulaOneApp.Models
+{
+    public class AuthResult // To reply for an authentication request if sucessful provide Token if not provide errors
+    {
+        public string Token { get; set; }
+        public bool Result { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ FormulaOneApp/FormulaOneApp/Models/DTOs/UserRegisterationRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormulaOneApp.Models.DTOs
+{
+    public class UserRegisterationRequestDto //To accept registeration request from user
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
