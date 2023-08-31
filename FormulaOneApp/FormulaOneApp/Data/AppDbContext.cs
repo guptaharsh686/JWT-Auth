977768726df9997ba39ddea019c2298ebed465ce
Harsh Gupta <guptaharsh686@gmail.com>
2023-08-31 23:42:22 +0530

    Added Entity Framework dependency configured AppDbContext class

---
+++ FormulaOneApp/FormulaOneApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FormulaOneApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormulaOneApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
