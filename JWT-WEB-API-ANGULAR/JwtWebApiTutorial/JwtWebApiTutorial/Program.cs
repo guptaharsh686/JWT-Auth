37bdeba7f96049db398506c69c00773bb5f23665
Harsh Gupta <guptaharsh686@gmail.com>
2023-09-10 16:15:59 +0530

    Added jwt bearer input field in Swagger UI

--- JWT-WEB-API-ANGULAR/JwtWebApiTutorial/JwtWebApiTutorial/Program.cs
+++ JWT-WEB-API-ANGULAR/JwtWebApiTutorial/JwtWebApiTutorial/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( opt =>
+{
+    opt.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standerd authorization header using the bearer scheme (\"bearer {token}\" )",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    opt.OperationFilter<SecurityRequirementsOperationFilter>();
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(opt =>
